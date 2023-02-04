a4a69d21831f17352588a5e68d5ca15546a3ee45
Anders Forsman <andeye@cyancore.net>
2023-02-04 14:11:54 +0200

    Add draft blob simulation

---
+++ Assets/Scripts/BlobsSim.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blobs : MonoBehaviour
+{
+    private List<GameObject> blobs = new List<GameObject>();
+    public GameObject BlobNode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Foo");
+        Blob a = new Blob("Bar");
+
+        for (int i = 0; i < 20; i++) {
+            GameObject newBlob = Instantiate(BlobNode, BlobNode.transform.position, Quaternion.identity);
+            newBlob.transform.Translate(
+                Random.Range(-8.0f, 8.0f),
+                Random.Range(-4.0f, 4.0f),
+                0);
+            blobs.Add(newBlob);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float f = 0;
+        foreach (GameObject blob in blobs) {
+            SphereCollider thisCollider = blob.GetComponent<SphereCollider>();
+            Collider[] hitColliders = Physics.OverlapSphere(
+                thisCollider.transform.position,
+                thisCollider.radius);
+            Vector3 newDirection = new Vector3(0, 0, 0);
+            foreach (Collider collider in hitColliders)
+            {
+                Vector3 collideDirection = blob.transform.position - collider.gameObject.transform.position;
+                newDirection += collideDirection;
+            }
+            newDirection = Vector3.ClampMagnitude(newDirection, 1);
+
+            blob.transform.Translate(newDirection * Time.deltaTime);
+        }
+    }
+}
