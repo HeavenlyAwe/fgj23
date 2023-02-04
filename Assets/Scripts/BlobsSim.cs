75acb6495a59b9ef0c2de65579efb9a895df0a72
Anders Forsman <andeye@cyancore.net>
2023-02-04 18:00:34 +0200

    Improve blob movement and add walls
    Magnitude property is for debugging.

--- Assets/Scripts/BlobsSim.cs
+++ Assets/Scripts/BlobsSim.cs
@@ -6,13 +6,16 @@ public class BlobSim : MonoBehaviour
 {
     private List<GameObject> blobs = new List<GameObject>();
     public GameObject BlobNode;
+    public float Clamping = 1f;
+    public float Friction = 0.8f;
+    public Vector3 SpawnPoint = new Vector3(0, 0, 0);
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < 20; i++) {
-            GameObject newBlob = Instantiate(BlobNode, BlobNode.transform.position, Quaternion.identity);
+            GameObject newBlob = Instantiate(BlobNode, SpawnPoint, Quaternion.identity);
             newBlob.transform.Translate(
-                Random.Range(-8.0f, 8.0f),
+                Random.Range(-2.5f, 2.5f),
                 Random.Range(-4.0f, 4.0f),
                 0);
             blobs.Add(newBlob);
@@ -31,12 +34,30 @@ public class BlobSim : MonoBehaviour
             Vector3 newDirection = new Vector3(0, 0, 0);
             foreach (Collider collider in hitColliders)
             {
-                Vector3 collideDirection = blob.transform.position - collider.gameObject.transform.position;
-                newDirection += collideDirection;
+                Blob blobComponent = collider.gameObject.GetComponent<Blob>();
+                Vector3 collideDirection;
+                if (blobComponent != null) {
+                    // Closest point doesn't work if spheres already intersect too much, then they just merge
+                    collideDirection = blob.transform.position - collider.gameObject.transform.position;
+                } else {
+                    // Object position doesn't work with walls. We want to move away from wall and not from the (possibly) far away origin
+                    collideDirection = blob.transform.position - collider.ClosestPoint(blob.transform.position);
+                }
+                float magnitude = collideDirection.magnitude;
+                if (magnitude == 0) {
+                    magnitude = 0.001f;
+                }
+                float inverse = 1 / magnitude;
+                newDirection += Vector3.Scale(collideDirection, new Vector3(inverse, inverse, inverse));
             }
-            newDirection = Vector3.ClampMagnitude(newDirection, 1);
+            newDirection = Vector3.ClampMagnitude(newDirection, Clamping);
+            Vector3 velocity = blob.GetComponent<Blob>().velocity;
+            velocity += newDirection;
+            velocity *= Friction;// * Time.deltaTime;
 
-            blob.transform.Translate(newDirection * Time.deltaTime);
+            blob.transform.Translate(velocity * Time.deltaTime);
+            blob.GetComponent<Blob>().velocity = velocity;
+            blob.GetComponent<Blob>().magnitude = Mathf.Round(velocity.magnitude*100);
         }
     }
 }
