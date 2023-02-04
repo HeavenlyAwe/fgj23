a095e966aab818670e9cba47b51490820989796d
CurlyNikolai <emil.levo@outlook.com>
2023-02-04 14:12:33 +0200

    added blob component to metaball

---
+++ Assets/Scripts/Blob.cs
@@ -0,0 +1,10 @@
+using GraphTools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blob : MonoBehaviour
+{
+    public Node node;
+    public Vector3 velocity;
+}
